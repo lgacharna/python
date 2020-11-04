d4ab6e3efb1468bde25b6a1d44226f854259b5bf
lgacharna <73554010+lgacharna@users.noreply.github.com>
2020-11-04 16:36:46 -0600

    Update debugCommentDrills.py

--- comments/debugCommentDrills.py
+++ comments/debugCommentDrills.py
@@ -12,17 +12,18 @@ volumeCalculator: 2 errors
 '''
 The goal of this function is to calculate the volume of
 an object, when the user inputs height, width, and depth.
-
+'''
 The function should print the sentence plus the volume and
-return the volume.
+
+#return the volume.
 
 def volumeCalculator(height, width, depth):
     area = height * width
     volume = depth * area
     sentence = "The volume of this object is: "
-    This prints out the sentence with the calculated volume.
+    #This prints out the sentence with the calculated volume.
     print(sentence + volume)
-    return volume
+    #return volume
 
 #Leave the next line alone
 volumeCalculator(5, 5, 5)
