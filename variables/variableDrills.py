1fb202a21f9c793eb8671595cbe6716b7b4c0e93
lgacharna <73554010+lgacharna@users.noreply.github.com>
2020-11-04 17:38:55 -0600

    variables drills

---
+++ variables/variableDrills.py
@@ -0,0 +1,80 @@
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a variable called name and assign it to your name. And print the variable.
+from builtins import False, True
+name = "Michael"
+print(name)
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+name = "Laura"
+'''INTEGERS'''
+var1 = 4
+print(var1)
+var2 = 10
+print(var2)
+var3 = var2
+print(var3)
+varX = 24
+print(varX)
+varY = 100
+print(varY)
+
+'''FLOATS'''
+Z = 1.1
+print(Z)
+A = 2.75
+print(A)
+B = A
+print(B)
+C = 100.1
+print(C)
+D = 12.3
+print(D)
+
+'''STRINGS'''
+
+name = "laura"
+print(name)
+e = "l"
+print(e)
+f = "a"
+print(f)
+g = "chicken"
+print(g)
+h = "lau"
+print(h)
+
+'''BOOLEANS'''
+
+done = False
+print(done)
+smart = True
+print(smart)
+i = done
+print(i)
+j = False
+print(j)
+almostDone = True
+print(almostDone)
+
+
+'''LISTS'''
+
+listInt = [var1, var2, var3, x, y]
+print[var3]
+listStrings = [name, e, f, g, h]
+print[g]
+listBooleans = [done, smart, i, j, almostDone]
+print[done]
+print[smart]
+print[i]
+print[j] 
+print[almostDone]
