423d0065da0404b7d8bbb510eaf1599f60b46ca9
lgacharna <73554010+lgacharna@users.noreply.github.com>
2020-11-04 18:12:17 -0600

    comment outline

---
+++ comments/commentsOutline.py
@@ -0,0 +1,24 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+'''
+#hello there
+
+'''
+
+'''
+
+jaslkjdklas
+adkjfkld
+ksdfhl
+dlkkjljsdf
+
+'''
+'''
+
+var = 4 #hello there
+print(var)
+
+'''
